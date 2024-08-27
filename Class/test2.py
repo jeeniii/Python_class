2a95e5b48576665ad1e0aaa5cddd122beaf3e941
DESKTOP-5SQNL29\xkr12 <xkr123456@gmail.com>
2024-08-27 16:12:17 +0900

    0827 업로드

---
+++ Class/test2.py
@@ -0,0 +1,27 @@
+# test1 = int('2')
+# test2 = float('2.5')
+# test3 = str(5)
+
+# print(test1)
+# print(test2)
+# print(test3)
+
+a = 'holo'
+b = ' '
+c = ''
+d = 0
+
+# test1 = int(float('2.5'))
+test2 = bool(a)
+test3 = bool(b)
+test4 = bool(c)
+test5 = bool(d)
+'''dddddddddd
+dddd
+ddddd'''
+# print(test1)
+print(test2)
+print(test3)
+print(test4)
+print(test5)
+print(bool(None))
