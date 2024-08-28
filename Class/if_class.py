1c7aca8c8598466be0072a2a906e38951bcb259a
DESKTOP-5SQNL29\xkr12 <xkr123456@gmail.com>
2024-08-28 11:41:05 +0900

    if, for

--- Class/if_class.py
+++ Class/if_class.py
@@ -1,1 +1,31 @@
 # If
+today = '토요일'
+# if today == '일요일' :
+#     print('게임 하쉴?')
+# print('공부 시작')
+
+# if today == '일요일' :
+#     print('게임 하쉴?')
+# else : 
+#     print('폰 겜 허쉴?')
+# print('공부 시작')
+
+if today == '일요일' :
+    print('게임 하쉴?')
+elif today == '토요일' : 
+    print('물 한잔 허쉴?')
+else : 
+    print('폰 겜 허쉴?')
+print('공부 시작')
+
+# If 중첩
+yellow_card = 0
+foul = True
+if foul:
+    yellow_card += 1
+    if yellow_card == 2:
+        print('퇴장')
+    else:
+        print('조심')
+else:
+    print('주의')
