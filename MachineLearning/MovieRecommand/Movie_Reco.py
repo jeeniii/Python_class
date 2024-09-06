d768af16b0c4613b35019462299beab959fccc17
DESKTOP-5SQNL29\xkr12 <xkr123456@gmail.com>
2024-09-07 00:42:32 +0900

    무비 프로젝트

--- MachineLearning/MovieRecommand/Movie_Reco.py
+++ MachineLearning/MovieRecommand/Movie_Reco.py
@@ -4,5 +4,13 @@ import numpy as np
 credits_data = pd.read_csv('C:\\Python\\Python_Class\\MachineLearning\\tmdb_5000\\tmdb_5000_credits.csv')
 movies_data = pd.read_csv('C:\\Python\\Python_Class\\MachineLearning\\tmdb_5000\\tmdb_5000_movies.csv')
 
-print(credits_data.head())
-print(credits_data[:3])
+# print(credits_data.head())
+# print(credits_data[:3])
+
+print(credits_data.shape)
+print(movies_data.shape)
+
+print(credits_data['title'].equals(movies_data['title']))
+print(credits_data.columns) 
+credits_data.columns = ['id', 'title', 'cast', 'crew']
+print(credits_data.columns) 
