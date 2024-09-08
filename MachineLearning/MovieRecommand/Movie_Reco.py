6503fd56b5316379971f389781ae9bf5f8e2f843
DESKTOP-5SQNL29\xkr12 <xkr123456@gmail.com>
2024-09-09 00:02:20 +0900

    인구통계적 기반

--- MachineLearning/MovieRecommand/Movie_Reco.py
+++ MachineLearning/MovieRecommand/Movie_Reco.py
@@ -13,4 +13,26 @@ print(movies_data.shape)
 print(credits_data['title'].equals(movies_data['title']))
 print(credits_data.columns) 
 credits_data.columns = ['id', 'title', 'cast', 'crew']
-print(credits_data.columns) 
+print(credits_data.columns)
+print(movies_data.columns)
+
+C = movies_data['vote_average'].mean()
+# print(C)
+m = movies_data['vote_count'].quantile(0.9)
+# print(m)
+
+q_movies = movies_data.copy().loc[movies_data['vote_count'] >= m ]
+print(q_movies.shape)
+q_movies['vote_count'].sort_values()
+print(q_movies['vote_count'])
+
+def weighted_rating(x, m=m, C=C):
+    v = x['vote_count']
+    R = x['vote_average']
+    return (v / ( v + m ) * R ) + (m / (m + v) * C)
+
+q_movies['score'] = q_movies.apply(weighted_rating, axis = 1)
+# print(q_movies.head(3))
+
+q_movies = q_movies.sort_values('score', ascending=False)
+print(q_movies[['title', 'vote_average', 'vote_count', 'score']].head(10))
